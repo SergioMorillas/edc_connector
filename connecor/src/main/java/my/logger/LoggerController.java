3d895a26f202603527a2ad6f864d02698cbe059b
sergio <smorillas@arsys.es>
2024-05-16 12:02:23 +0200

    Modificados los certificados, la configuracion y los scripts de prueba de transferencia

--- connecor/src/main/java/my/logger/LoggerController.java
+++ connecor/src/main/java/my/logger/LoggerController.java
@@ -15,65 +15,63 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 
-@Consumes({MediaType.WILDCARD})
-@Produces({MediaType.WILDCARD})
+@Consumes({MediaType.APPLICATION_JSON})// La clase solo puede recibir peticiones de tipo application/json
+@Produces({MediaType.APPLICATION_JSON})// La clase solo puede devolver peticiones de tipo application/json
 @Path("/")
 public class LoggerController {
 
-    private final Monitor monitor;
+    private final Monitor monitor; // Es el logger, se lo pasa la clase Extension 
 
     public LoggerController(Monitor monitor) {
         this.monitor = monitor;
     }
 
-    @POST
+    @POST // Para obtener peticiones POST, tambien pueden ser GET, PUT, PATCH... 
     @Path("logger")
-    public String endppoint(String body) {
+    public String  endppoint(String body) {// Creamos un endpoint que recibe un JSON como cadena de texto
         ObjectMapper mapper;
         JsonNode jsonNode;
-        StringBuilder datos = new StringBuilder();
-        String aux="";
         try {
             mapper = new ObjectMapper();
-            jsonNode = mapper.readTree(body);
+            jsonNode = mapper.readTree(body);//Aquí creamos los nodos del JSON
+            //Ahora le quitamos las comillas a los campos del JSON que queremos utilizar, ya que sino
+            //hiciesemos esto los valores con las comillas no serían utilies
             String endpoint=jsonNode.get("endpoint").toString().replaceAll("\"","");
             String authCode = jsonNode.get("authCode").toString().replaceAll("\"","");
             String id = jsonNode.get("id").toString().replaceAll("\"","");
+            // Utilizamos el monitos para mostrar mensajes informativos
             monitor.info("El valor del auth code es: " + authCode);
-
-
+            // Accedemos al endpoint que nos ha proporcionado el proveedor en el JSON con el authcode como cabecera de la petición, aceptando tanto input como output
             URL url = new URL(endpoint);
             System.out.println(url);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setDoOutput(true);
             con.setDoInput(true);
             con.setRequestMethod("GET");
-
             con.setRequestProperty("Content-Length", "" + authCode.getBytes().length);
             con.setRequestProperty("Authorization", authCode);
-            try{
-                DataInputStream rd = new DataInputStream(con.getInputStream());
 
-                OutputStream fos = new FileOutputStream("./"+id);
-                fos.write(rd.readAllBytes());
+            try{
+                DataInputStream rd = new DataInputStream(con.getInputStream()); // Aquí utilizo un DataInputStream para poder leer datos binarios
+
+                OutputStream fos = new FileOutputStream("./"+id); // Creo un OutputStream (Que tambien admite binarios) que cree un fichero con el id de la transferencia como nombre 
+                fos.write(rd.readAllBytes()); // Leemos la fuente de datos externa y la escribimos en el fichero interno
                 fos.flush();
 
             }catch (Exception ignored){
-
+                monitor.severe("Excepción leyendo los datos o escribiendolos", ignored);
             }
 
         } catch (JsonProcessingException e) {
-            monitor.info("El code es: " +body);
             monitor.severe("El body no era un JSON con el formato correcto");
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        return "{\"response\":\"download correctly\"}";
+        // Si ha llegado hasta aquí y no ha saltado excepción devuelvo que ha sido correcto
+        return "{\"respuesta\":\"descarga correcta\"}";
     }
 }
--- connecor/src/main/java/org/example/DownloadController.java
+++ connecor/src/main/java/org/example/DownloadController.java
@@ -3,23 +3,20 @@ package org.example;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.net.httpserver.HttpContext;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.edc.spi.monitor.Monitor;
-
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Collections;
 
 
-@Consumes({MediaType.WILDCARD})
-@Produces({MediaType.WILDCARD})
+@Consumes({MediaType.APPLICATION_JSON}) // La clase solo puede recibir peticiones de tipo application/json
+@Produces({MediaType.APPLICATION_JSON}) // La clase solo puede devolver peticiones de tipo application/json
 @Path("/")
 public class DownloadController {
 
-    private final Monitor monitor;
+    private final Monitor monitor; // Es el logger, se lo pasa la clase Extension 
 
     public DownloadController(Monitor monitor) {
         this.monitor = monitor;
@@ -27,14 +24,14 @@ public class DownloadController {
 
     @POST
     @Path("endpoint")
-    public String endppoint(String body) {
+    public String endppoint(String body) { // Creamos un endpoint que recibe un JSON como cadena de texto
         ObjectMapper mapper;
         JsonNode jsonNode = null;
 
         try {
             mapper = new ObjectMapper();
-            jsonNode = mapper.readTree(body);
-            OutputStream fos = new FileOutputStream("./"+jsonNode.fieldNames().next()+".json");
+            jsonNode = mapper.readTree(body); //Aquí creamos los nodos del JSON
+            OutputStream fos = new FileOutputStream("./"+jsonNode.fieldNames().next()+".json"); 
             fos.write(body.getBytes(StandardCharsets.UTF_8));
 
         } catch (JsonProcessingException e) {
